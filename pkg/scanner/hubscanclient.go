2b7ea5dd2aa8852f7833bb6ee3d482b5bf8aae3d
Matt Fenwick <mfenwick100@gmail.com>
2018-03-02 11:18:18 -0500

    remove -v from scan client invocation

--- pkg/scanner/hubscanclient.go
+++ pkg/scanner/hubscanclient.go
@@ -87,7 +87,6 @@ func (hsc *HubScanClient) Scan(job ScanJob) error {
 		"--username", hsc.username,
 		"--name", job.HubScanName,
 		"--insecure",
-		"-v",
 		path)
 
 	log.Infof("running command %+v for image %s\n", cmd, job.Sha)
